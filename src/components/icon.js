f8d36d9e97394aed391d079b3a610d7448f4e212
Manuel Monterosso <madhutter@live.it>
2019-02-08 00:45:00 +0100

    Accessibility fixes

--- src/components/icon.js
+++ src/components/icon.js
@@ -7,13 +7,15 @@ const Icon = props => {
   const { d, size = '1em', label, style: styles } = props
 
   return (
-    <span className={style.root} style={styles} role="figure">
+    <span className={style.root} style={styles} role="img" aria-label="Hello Friend">
       <svg
         version="1.1"
         width={size}
         height={size}
         viewBox="0 0 48 48"
         xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-label="Hello Friend logo"
       >
         <path d={d} className={style.icon} />
       </svg>
