2966410b59ca9aa00c61768f19abbce163731a65
panr <radoslaw.koziel@gmail.com>
2019-02-23 00:43:56 +0100

    Refactor Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { Helmet } from 'react-helmet'
@@ -7,99 +7,77 @@ import Menu from './menu'
 
 import style from '../styles/header.module.css'
 
-class Header extends React.Component {
-  state = {
-    userTheme:
-      (typeof window !== 'undefined' && window.localStorage.getItem('theme')) ||
-      null,
-    isMobileMenuVisible: false,
-    isSubMenuVisible: false,
-  }
-
-  onChangeTheme = this.onChangeTheme.bind(this)
-
-  onToggleMobileMenu = this.onToggleMobileMenu.bind(this)
-
-  onToggleSubMenu = this.onToggleSubMenu.bind(this)
-
-  onChangeTheme() {
-    const { userTheme } = this.state
-    const { defaultTheme } = this.props
-
+const Header = props => {
+  const {
+    siteLogo,
+    logoText,
+    siteTitle,
+    mainMenu,
+    mainMenuItems,
+    menuMoreText,
+    defaultTheme,
+  } = props
+  const defaultThemeState =
+    (typeof window !== 'undefined' && window.localStorage.getItem('theme')) ||
+    null
+  const [userTheme, changeTheme] = useState(defaultThemeState)
+  const [isMobileMenuVisible, toggleMobileMenu] = useState(false)
+  const [isSubMenuVisible, toggleSubMenu] = useState(false)
+  const onChangeTheme = () => {
     const opositeTheme =
       (userTheme || defaultTheme) === 'light' ? 'dark' : 'light'
 
-    this.setState({ userTheme: opositeTheme })
+    changeTheme(opositeTheme)
+
     typeof window !== 'undefined' &&
       window.localStorage.setItem('theme', opositeTheme)
   }
+  const onToggleMobileMenu = () => toggleMobileMenu(!isMobileMenuVisible)
+  const onToggleSubMenu = () => toggleSubMenu(!isSubMenuVisible)
 
-  onToggleMobileMenu() {
-    const { isMobileMenuVisible } = this.state
-    this.setState({ isMobileMenuVisible: !isMobileMenuVisible })
-  }
-
-  onToggleSubMenu() {
-    const { isSubMenuVisible } = this.state
-    this.setState({ isSubMenuVisible: !isSubMenuVisible })
-  }
-
-  render() {
-    const {
-      siteLogo,
-      logoText,
-      siteTitle,
-      mainMenu,
-      mainMenuItems,
-      menuMoreText,
-      defaultTheme,
-    } = this.props
-    const { userTheme, isSubMenuVisible, isMobileMenuVisible } = this.state
-
-    return (
-      <>
-        <Helmet>
-          <title>{siteTitle}</title>
-          <body
-            className={
-              (userTheme || defaultTheme) === 'light'
-                ? 'light-theme'
-                : 'dark-theme'
-            }
-          />
-        </Helmet>
-        <header className={style.header}>
-          <div className={style.inner}>
-            <Link to="/">
-              <div className={style.logo}>
-                {siteLogo.src ? (
-                  <img src={siteLogo.src} alt={siteLogo.alt} />
-                ) : (
-                  <>
-                    <span className={style.mark}>></span>
-                    <span className={style.text}>{logoText}</span>
-                    <span className={style.cursor} />
-                  </>
-                )}
-              </div>
-            </Link>
-            <span className={style.right}>
-              <Menu
-                mainMenu={mainMenu}
-                mainMenuItems={mainMenuItems}
-                isMobileMenuVisible={isMobileMenuVisible}
-                isSubMenuVisible={isSubMenuVisible}
-                menuMoreText={menuMoreText}
-                onToggleMobileMenu={this.onToggleMobileMenu}
-                onToggleSubMenu={this.onToggleSubMenu}
-                onChangeTheme={this.onChangeTheme}
-              />
-            </span>
-          </div>
-        </header>
-      </>
-    )
-  }
+  return (
+    <>
+      <Helmet>
+        <title>{siteTitle}</title>
+        <body
+          className={
+            (userTheme || defaultTheme) === 'light'
+              ? 'light-theme'
+              : 'dark-theme'
+          }
+        />
+      </Helmet>
+      <header className={style.header}>
+        <div className={style.inner}>
+          <Link to="/">
+            <div className={style.logo}>
+              {siteLogo.src ? (
+                <img src={siteLogo.src} alt={siteLogo.alt} />
+              ) : (
+                <>
+                  <span className={style.mark}>></span>
+                  <span className={style.text}>{logoText}</span>
+                  <span className={style.cursor} />
+                </>
+              )}
+            </div>
+          </Link>
+          <span className={style.right}>
+            <Menu
+              mainMenu={mainMenu}
+              mainMenuItems={mainMenuItems}
+              isMobileMenuVisible={isMobileMenuVisible}
+              isSubMenuVisible={isSubMenuVisible}
+              menuMoreText={menuMoreText}
+              onToggleMobileMenu={onToggleMobileMenu}
+              onToggleSubMenu={onToggleSubMenu}
+              onChangeTheme={onChangeTheme}
+            />
+          </span>
+        </div>
+      </header>
+    </>
+  )
 }
 
 Header.propTypes = {
